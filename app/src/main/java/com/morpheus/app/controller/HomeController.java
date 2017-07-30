6f038486b3b237bbf250b4dacb7a11b0c0005977
Rodrigo Oda <rodrigooda@outlook.com>
2017-07-30 14:27:27 -0300

    alteração de path em HomeController

--- app/src/main/java/com/morpheus/app/controller/HomeController.java
+++ app/src/main/java/com/morpheus/app/controller/HomeController.java
@@ -2,11 +2,12 @@ package com.morpheus.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String goToHome() {
 		return "index";
 	}
