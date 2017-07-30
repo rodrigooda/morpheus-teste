d8d38032eec68ff6202410898153e6bc834e82cd
Rodrigo Oda <rodrigooda@outlook.com>
2017-07-30 17:13:51 -0300

    Exceptions alterações

--- app/src/main/java/com/morpheus/app/controller/RestResponseEntityExceptionHandler.java
+++ app/src/main/java/com/morpheus/app/controller/RestResponseEntityExceptionHandler.java
@@ -11,20 +11,25 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.morpheus.app.exception.AppException;
+import com.morpheus.app.exception.ExceptionResponse;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-
+	
 	@ExceptionHandler(IOException.class)
-	protected ResponseEntity<Object> handleIOException(RuntimeException ex, WebRequest request) {
-		String bodyOfResponse = "IOException: Contate o administrador do sistema";
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	protected ResponseEntity<ExceptionResponse> handleIOException(IOException ex, WebRequest request) {
+		ExceptionResponse er = new ExceptionResponse();
+		er.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		er.setDescription(ex.getMessage());
+		return new ResponseEntity<ExceptionResponse>(er, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(AppException.class)
-	protected ResponseEntity<Object> handleAppException(RuntimeException ex, WebRequest request) {
-		String bodyOfResponse = "AppException: Contate o administrador do sistema";
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	protected ResponseEntity<ExceptionResponse> handleAppException(AppException ex, WebRequest request) {
+		ExceptionResponse er = new ExceptionResponse();
+		er.setCode(HttpStatus.BAD_REQUEST.value());
+		er.setDescription(ex.getMessage());
+		return new ResponseEntity<ExceptionResponse>(er, HttpStatus.BAD_REQUEST);
 	}
 
 }
