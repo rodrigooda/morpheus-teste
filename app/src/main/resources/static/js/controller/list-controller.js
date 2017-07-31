1b4f4f3f77d953dcb5b4b3061baec91fe31fe3a7
Rodrigo Oda <rodrigooda@outlook.com>
2017-07-31 09:02:28 -0300

    Adicionado busca por nome

--- app/src/main/resources/static/js/controller/list-controller.js
+++ app/src/main/resources/static/js/controller/list-controller.js
@@ -2,7 +2,17 @@
 app.controller("ListController", [ '$scope', 'UsuarioService',
 		function($scope, UsuarioService) {
 			$scope.usuarios;
+			$scope.getAll = function(){
 			UsuarioService.getAll().then(function(response) {
 				$scope.usuarios = response.data;
 			});
+			};
+			$scope.search = function() {
+				UsuarioService.getByNome($scope.nome).then(function(response) {
+					$scope.usuarios = [ response.data ];
+				}, function(error) {
+					$scope.usuarios = [ ];
+				});
+			};
+			$scope.getAll();
 		} ]);
